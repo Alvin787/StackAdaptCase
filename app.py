868bc74811b63c0f677300ca99a8843a308e3b82
Alvin Qian <alvinhalo4@gmail.com>
2023-09-28 14:05:24 -0400

    finished all graphs

--- app.py
+++ app.py
@@ -2,22 +2,19 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
-MONTHS = ( "2019-09-01",
-    "2019-10-01",
-    "2019-11-01",
-    "2019-12-01",
-    "2020-01-01",
-    "2020-02-01")
-
 st.set_page_config(page_title="StackAdapt Case Dashboard", page_icon=":bar_chart:", layout="wide")
 
-df = pd.read_excel(
-    io="filteredExcel.xlsx",
-    engine="openpyxl",
-    sheet_name="Question_caseFiltered",
-    nrows=16000,
-)
+@st.cache_data
+def get_data_from_excel():
+    df = pd.read_excel(
+        io="filteredExcel.xlsx",
+        engine="openpyxl",
+        sheet_name="Question_caseFiltered",
+        nrows=16000,
+    )
+    return df
 
+df = get_data_from_excel()
 
 # ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
@@ -60,52 +57,80 @@ with left_column:
     st.subheader("Total Spent:")
     st.subheader(f"$ {totalSpent:,}")
 with middle_column:
-    st.subheader("Conversions:")
+    st.subheader("Total Conversions:")
     st.subheader(f"{conversions}")
 with middle2_column:
-    st.subheader("Clicks:")
+    st.subheader("Total Clicks:")
     st.subheader(f"{clicks}")  
 with right_column:
-    st.subheader("CTR:")
+    st.subheader("Average CTR:")
     st.subheader(f"{average_CTR}")
 
 st.markdown("""---""")
 
 
-custom_month_order = [
-    "2019 Sep 01",
-    "2019 Oct 01",
-    "2019 Nov 01",
-    "2019 Dec 01",
-    "2020 Jan 01",
-    "2020 Feb 01"
-]
-# df_display = df_selection[df_selection['format'] == 'display']
+df_plot = df_selection[df_selection['format'] == 'native'].groupby('month')['conversions'].sum().reset_index()
+df_plot['month'] = pd.to_datetime(df_plot['month'], format='%Y %b %d', errors='coerce')
+df_plot = df_plot.sort_values(by="month")
+fig = px.line(df_plot, x='month', y=['conversions', ], title='Conversions Over Months (Native Format)')
 
-# df_plot = df_display.groupby('month')['conversions'].sum()
 
+conversionsByCategory = df_selection.groupby(by=["advertiser_category"])[["conversions"]].sum().sort_values(by="conversions")
+fig2 = px.bar(
+    conversionsByCategory,
+    x="conversions",
+    y=conversionsByCategory.index,
+    orientation="h",
+    title="<b>Category vs Conversion</b>",
+    labels={
+        "conversions": "Conversions",
+        "advertiser_category": "Category",
+            },
+    template="plotly_white",
+)
 
 df_plot = df_selection[df_selection['format'] == 'display'].groupby('month')['conversions'].sum().reset_index()
+df_plot['month'] = pd.to_datetime(df_plot['month'], format='%Y %b %d', errors='coerce')
+df_plot = df_plot.sort_values(by="month")
+fig3 = px.line(df_plot, x='month', y=['conversions', ], title='Conversions Over Months (Display Format)')
+
+df_plot = df_selection[df_selection['format'] == 'video'].groupby('month')['conversions'].sum().reset_index()
+df_plot['month'] = pd.to_datetime(df_plot['month'], format='%Y %b %d', errors='coerce')
+df_plot = df_plot.sort_values(by="month")
+fig5 = px.line(df_plot, x='month', y=['conversions', ], title='Conversions Over Months (Video Format)')
+
+# Calculate CTR per month
+df_plot = df_selection.groupby('month')['CTR'].mean().reset_index()
+df_plot['month'] = pd.to_datetime(df_plot['month'], format='%Y %b %d', errors='coerce')
+df_plot = df_plot.sort_values(by="month")
+
+# Create a line graph for CTR
+fig_ctr = px.line(df_plot, x='month', y='CTR', title='Average Click-Through Rate (CTR) Over Months')
 
 
-fig = px.line(df_plot, x='month', y='conversions', title='Conversions Over Months (Display Format)')
-# fig.update_xaxes(categoryorder='array', categoryarray = [
-#     "2019 Sep 01",
-#     "2019 Oct 01",
-#     "2019 Nov 01",
-#     "2019 Dec 01",
-#     "2020 Jan 01",
-#     "2020 Feb 01"
-# ])
+df_plot = df_selection.groupby('month')['spend'].sum().reset_index()
+df_plot['month'] = pd.to_datetime(df_plot['month'], format='%Y %b %d', errors='coerce')
+df_plot = df_plot.sort_values(by="month")
+spend = px.line(df_plot, x='month', y='spend', title='Spending Over Months')
 
-left_column, right_column = st.columns(2)
+
+left_column, right_column= st.columns(2)
 left_column.plotly_chart(fig, use_container_width=True)
-right_column.plotly_chart(fig, use_container_width=True)
+right_column.plotly_chart(fig2, use_container_width=True)
+left_column.plotly_chart(fig3, use_container_width=True)
+right_column.plotly_chart(fig_ctr, use_container_width=True)
+left_column.plotly_chart(fig5, use_container_width=True)
+right_column.plotly_chart(spend, use_container_width=True)
 
 
-
-
-
-
-# print(df)
 st.dataframe(df_selection)
+
+
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
